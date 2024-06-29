850d448c864c987d4f80907d0d22fe5aebb2dcbc
RoterKrug <rotterkrug@gmail.com>
2024-06-29 17:06:00 -0600

    memide.php

---
+++ memide.php
@@ -0,0 +1,12 @@
+<?php
+   $respuestas = array(
+       "vaya titan que tienes"
+       ,"casi tan largo como la torre eiffel"
+       ,"que buen buque de guerra tienes"
+       ,"Mehh"
+       ,"ni cosquillas hace esa madre"
+       ,"mas pequeño que un atomo"
+       );
+   $random = array_rand($respuestas);
+   echo $respuestas[$random];
+?>
